1b86b43bc375d64e2cfd3f6a40ed4046f8308df4
Leonardo Dias <Ldias@gap.com>
2021-09-18 17:33:11 -0300

    Definir calendario livre #8

--- backend/src/test/java/br/com/agendaquiro/domain/calendar/AgendaBuilderTest.java
+++ backend/src/test/java/br/com/agendaquiro/domain/calendar/AgendaBuilderTest.java
@@ -52,4 +52,48 @@ public class AgendaBuilderTest {
             assertThat(endTime).isEqualTo(currentDateTimeVerification.toLocalTime());
         }
     }
+
+    @Test
+    public void shouldBuildAgendaWithLunchBlocked() {
+        //GIVEN A BLOCKED DAYS AND TIMES CONFIGURATION
+        DaysOfTheWeeksBlocked daysOfTheWeeksBlocked = new DaysOfTheWeeksBlockedBuilder()
+                .blockAllDays(LocalTime.of(18,0), LocalTime.of(23,59))
+                .blockAllDays(LocalTime.of(00,00), LocalTime.of(10,0))
+                .blockAllDays(LocalTime.of(12,00), LocalTime.of(13,0))
+                .blockSunday()
+                .build();
+
+        //GIVEN A TIME PERIOD OF AGENDA (sunday-blocked, monday-free (10h-18))
+        LocalDateTime mondayStart = LocalDateTime.of(2021, 9, 20, 00, 00);
+        LocalDateTime tuesdayStart = LocalDateTime.of(2021, 9, 21, 00, 00);
+        //GIVEN A PERIOD OF TIME OF 60 min
+        int durationInMinutes = 60;
+
+        //WHEN ASKED TO CREATE AGENDA
+        AgendaOfPeriod agendaOfPeriod = new AgendaOfPeriodBuilder()
+                .period(durationInMinutes, mondayStart, tuesdayStart)
+                .daysOfWeekBlocked(daysOfTheWeeksBlocked)
+                .build();
+
+        //THEN the agenda should be fine
+        List<PeriodSlot> periodSlots = agendaOfPeriod.getPeriodSlots();
+
+        //THEN SHOULD HAVE CREATE 8 slots
+        assertThat(periodSlots.size()).isEqualTo(7);
+        LocalDateTime currentDateTimeVerification =  LocalDateTime.of(2021, 9, 20, 10, 00);
+        for (PeriodSlot periodSlot : periodSlots) {
+            LocalDate date = periodSlot.getDate();
+            LocalTime startTime = periodSlot.getStartTime();
+            LocalTime endTime = periodSlot.getEndTime();
+            //AVOID lunch time
+            if (currentDateTimeVerification.toLocalTime().equals(LocalTime.of(12, 0))) {
+                currentDateTimeVerification = currentDateTimeVerification.plusMinutes(durationInMinutes);
+            }
+            assertThat(date).isEqualTo(currentDateTimeVerification.toLocalDate());
+            assertThat(startTime).isEqualTo(currentDateTimeVerification.toLocalTime());
+
+            currentDateTimeVerification = currentDateTimeVerification.plusMinutes(durationInMinutes);
+            assertThat(endTime).isEqualTo(currentDateTimeVerification.toLocalTime());
+        }
+    }
 }
