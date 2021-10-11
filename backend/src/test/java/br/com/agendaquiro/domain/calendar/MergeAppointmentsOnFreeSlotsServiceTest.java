f581cd143b53f5e4c6077e3d9b0e16d807dd5e92
Leonardo Dias <Ldias@gap.com>
2021-10-11 10:40:36 -0300

    shouldMergeAppointmentsOnFreeSlotsService FAILING

---
+++ backend/src/test/java/br/com/agendaquiro/domain/calendar/MergeAppointmentsOnFreeSlotsServiceTest.java
@@ -0,0 +1,61 @@
+package br.com.agendaquiro.domain.calendar;
+
+import br.com.agendaquiro.domain.freeappointmentsslots.PeriodSlot;
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MergeAppointmentsOnFreeSlotsServiceTest {
+
+    private MergeAppointmentsOnFreeSlotsService service;
+
+    @Test
+    public void shouldMergeAppointmentsOnFreeSlotsService() {
+        service = new MergeAppointmentsOnFreeSlotsService();
+        //GIVEN A FREE SLOTS
+        List<PeriodSlot> freeSlots = new ArrayList<>();
+        PeriodSlot nonConflictedSlotA = PeriodSlot.builder()
+                .date(LocalDate.of(2021, 10, 11))
+                .startTime(LocalTime.of(13, 00))
+                .endTime(LocalTime.of(14, 00))
+                .build();
+        freeSlots.add(nonConflictedSlotA);
+        PeriodSlot conflictFreeSlot = PeriodSlot.builder()
+                .date(LocalDate.of(2021, 10, 11))
+                .startTime(LocalTime.of(14, 00))
+                .endTime(LocalTime.of(15, 00))
+                .build();
+        freeSlots.add(conflictFreeSlot);
+        PeriodSlot nonConflictedSlotB = PeriodSlot.builder()
+                .date(LocalDate.of(2021, 10, 11))
+                .startTime(LocalTime.of(15, 00))
+                .endTime(LocalTime.of(16, 00))
+                .build();
+        freeSlots.add(nonConflictedSlotB);
+
+        //GIVEN an appointment
+        List<PeriodSlot> appointments = new ArrayList<>();
+        PeriodSlot appointment = PeriodSlot.builder()
+                .date(LocalDate.of(2021, 10, 11))
+                .startTime(LocalTime.of(14, 10))
+                .endTime(LocalTime.of(15, 00))
+                .build();
+        appointments.add(appointment);
+
+        //WHEN asked to merge
+        List<PeriodSlot> mergedSlots = service.merge(freeSlots, appointments);
+
+        //THEN
+        assertThat(mergedSlots).contains(appointment);
+        assertThat(mergedSlots).contains(nonConflictedSlotA);
+        assertThat(mergedSlots).contains(nonConflictedSlotB);
+        assertThat(mergedSlots).doesNotContain(conflictFreeSlot);
+
+    }
+
+}
