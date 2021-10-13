d3708b526caa513cf9c7b81076ee6ce194cb013a
Leonardo Dias <Ldias@gap.com>
2021-10-13 14:06:53 -0300

    Integration Test folder

---
+++ backend/src/integrationTest/java/CustomerIntegrationTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CustomerIntegrationTest {
+
+    @Test
+    public void test() {
+        assertThat(true).isTrue();
+    }
+}
